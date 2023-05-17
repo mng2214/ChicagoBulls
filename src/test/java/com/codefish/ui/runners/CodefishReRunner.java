79cf892d884121d96564e932962c88f0b868a1d1
mng2214 <108999655+mng2214@users.noreply.github.com>
2023-05-16 19:41:04 -0500

    adding blank project for team 1

---
+++ src/test/java/com/codefish/ui/runners/CodefishReRunner.java
@@ -0,0 +1,15 @@
+package com.codefish.ui.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "target/uiFailedTests.txt",
+        glue = "codefish/ui/stepdef",
+      //  tags = "@smoke , @regression",
+        plugin = {"pretty", "html:target/uiReport.html", "rerun:target/uiFailedTests.txt"}
+)
+public class CodefishReRunner {
+}
