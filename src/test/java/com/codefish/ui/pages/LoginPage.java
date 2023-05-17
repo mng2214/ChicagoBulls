1bc193c97261db6a549fdc4d6550ef15fcc400a9
mng2214 <108999655+mng2214@users.noreply.github.com>
2023-05-16 21:32:19 -0500

    testes positive and negative login

---
+++ src/test/java/com/codefish/ui/pages/LoginPage.java
@@ -0,0 +1,55 @@
+package com.codefish.ui.pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.ConfigReader;
+
+public class LoginPage {
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//input[@name='username']")
+    WebElement username;
+    @FindBy(xpath = "//input[@name='password']")
+    WebElement password;
+    @FindBy(xpath = "//button[@name='loginbtn']")
+    WebElement loginButton;
+    @FindBy(xpath = "//div[contains(text(),'Authentication failed')]")
+    WebElement errorMessage;
+    @FindBy(xpath = "//a[.='Logout']")
+    WebElement logoutButton;
+
+    public void login (){
+        this.username.clear();
+        this.username.sendKeys(ConfigReader.readProperty("username"));
+        this.password.clear();
+        this.password.sendKeys(ConfigReader.readProperty("username"));
+        loginButton.click();
+    }
+
+
+    public void loginValidateTitle(String expectedTitle, WebDriver driver) {
+        Assert.assertEquals(expectedTitle, driver.getTitle().trim());
+    }
+
+    public void logout() {
+        logoutButton.click();
+    }
+
+    public void loginNegative(String username, String password) {
+        this.username.clear();
+        this.username.sendKeys(username);
+        this.password.clear();
+        this.password.sendKeys(password);
+        loginButton.click();
+    }
+
+    public String loginValidateMessage() {
+        return errorMessage.getText();
+    }
+
+}
